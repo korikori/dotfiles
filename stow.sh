eca3a4ca0c870838c11c5f6af547a69189b4fce5
korikori <kori@tulibu-dibu.com>
2019-10-18 21:16:41 +0300

    dotfiles v2

---
+++ stow.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#dotfiles2, now with gnu stow
+stow zsh
+stow vim
+stow openbox
+stow fbpanel
+stow tint2
+stow dmenu
+stow terminator
+stow geany
+stow devilspie
+stow conky
+stow redshift
+
