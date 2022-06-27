8703bb7ddf959baf8e174538024680fe64416531
Prehistoric Dog <kori@tulibu-dibu.com>
2022-06-27 07:55:25 +0300

    Update dmenu-bind.sh

--- dmenu/.config/dmenu/dmenu-bind.sh
+++ dmenu/.config/dmenu/dmenu-bind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 termcmd="terminator -e" 
-browsercmd="firefox --profile /home/kori/.mozilla/firefox/p0a1q7i8.casual"
+browsercmd="firefox"
 cmd="$(dmenu_path | dmenu -fn 'Hack-8' -b -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617')" 
 case $cmd in 
 	\>* ) ${browsercmd} "https://duckduckgo.com/?q=$(echo $cmd | sed "s/>//")";; 
